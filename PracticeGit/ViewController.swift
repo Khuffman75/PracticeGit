bbe833ab0b731749ca2462a978fdfa611a597cef
Khuffman75 <Khuffman0905@gmail.com>
2020-08-12 09:52:03 -0700

    Initial Commit

---
+++ PracticeGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PracticeGit
+//
+//  Created by SVD on 8/12/20.
+//  Copyright © 2020 SVD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
